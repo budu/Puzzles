830989b50977fd9f683f0b27ff9147a4c68d1793
Nicolas Buduroi <nbuduroi@gmail.com>
2011-08-24 23:32:12 -0400

    Added a my_turn_in placeholder method with appropriate documentation.

---
+++ lib/snake_puzzle.rb
@@ -0,0 +1,22 @@
+
+module SnakePuzzle
+
+  # Returns the number of turn before my next turn in a snake draft.
+  #
+  # @param my_position [Integer] The position of my team.
+  # @param current_position [Integer] The position of the team currently
+  #   playing.
+  # @param current_direction [Symbol] The direction of the team
+  #   currently playing.
+  # @param size [Integer] The number of teams.
+  # @return [Integer] The number of turn before my next turn.
+  def my_turn_in(my_position,
+                 current_position,
+                 current_direction,
+                 size)
+
+    # TODO: make it work
+
+  end
+
+end
