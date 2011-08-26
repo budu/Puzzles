092682c5d8bcd9d882db6d4b9b9777a99c114bb9
Nicolas Buduroi <nbuduroi@gmail.com>
2011-08-26 09:25:48 -0400

    Improved Snake puzzles documentation.

--- lib/puzzles/snake.rb
+++ lib/puzzles/snake.rb
@@ -1,10 +1,28 @@
 
 module Puzzles
+
+  # A draft, where there's multiple managers that must pick players to
+  # make their team. Each have a turn in which they can make a pick. It
+  # follows a predetermined order, so each team is assigned a position
+  # from 1 to the number of team involved.
+  #
+  # A snake draft is a way to compensate the disavantage of being the
+  # last one to pick. The manager are given a turn in the order of their
+  # positions in the first pass, the this order is reversed, giving the
+  # last manager two picks in a row. This is repeated as long as there
+  # is an incomplete team.
+  #
+  # For example, in a snake draft having three teams, the positions for
+  # each turn is:
+  #
+  #  turn:      0  1  2  3  4  5  6  7  8  9...
+  #  position:  1  2  3  3  2  1  1  2  3  3...
   module Snake
 
     # Returns the number of turn before my next turn in a snake draft.
     #
-    # Position arguments starts at 1.
+    # Example:
+    #     my_turn_in 1, 2, :forward, 5     # returns 8
     #
     # @param my_position [Integer] The position of my team.
     # @param current_position [Integer] The position of the team currently
@@ -23,4 +41,5 @@ module Puzzles
     end
 
   end
+
 end
