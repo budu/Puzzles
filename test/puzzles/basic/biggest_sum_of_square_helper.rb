d598002344f4981156e4f030e86e7d545601ed5a
Nicolas Buduroi <nbuduroi@gmail.com>
2011-08-29 23:01:54 -0400

    Fixed assertion arguments order.

--- test/puzzles/basic/biggest_sum_of_square_helper.rb
+++ test/puzzles/basic/biggest_sum_of_square_helper.rb
@@ -4,8 +4,8 @@ class Test::Unit::TestCase
   include Puzzles::Basic
 
   def assert_biggest_sum_of_square(numbers, x, y = 0)
-    assert_equal(biggest_sum_of_square(*numbers),
-                 x * x + y * y,
+    assert_equal(x * x + y * y,
+                 biggest_sum_of_square(*numbers),
                  "Called biggest_sum_of_square with " +
                  "*#{numbers.inspect} as argument")
   end
